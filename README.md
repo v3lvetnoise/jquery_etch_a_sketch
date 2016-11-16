c21ab4a22520d2e2e447380a315e37f065cc1a81
Ivan S <v3lvetnoise@gmail.com>
2016-11-16 17:02:57 +0700

    Add clearfix, random color on hover and README file

---
+++ README.md
@@ -0,0 +1,2 @@
+Browser version of something between a sketchpad and an Etch-A-Sketch.
+Assignment for [The Odin Project](http://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery).
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,43 @@
 $(document).ready(function() {
-  var side = 16;
+  var squares_amount = prompt("How many squares do you want on each side? (64 max)");
 
-  generateGrid(side);
+  if (squares_amount < 1 || squares_amount == null || squares_amount > 64) {
+    squares_amount = 16;
+  }
+
+  generateGrid(squares_amount);
 
   $(".square").hover(function() {
-    $(this).css("background-color", "black").css("opacity", "1");
+    $(this).css("background-color", getRandomColor()).css("opacity", "1");
   });
 
   $("#generateNew").on("click", function() {
-    location.reload();
+    reloadPage();
   });
 
-  function generateGrid(side) {
-    var squares = side * side;
+  function generateGrid(squares_amount) {
+    var square_size = (800 / squares_amount).toString() + "px";
+    var squares = squares_amount * squares_amount;
+
     $("#grid").empty();
+
     for (i = 0; i < squares; i++) {
       $("#grid").append("<div class='square'></div>")
     }
+
+    $(".square").css({"width": square_size, "height": square_size});
   }
+
+  function reloadPage() {
+    location.reload();
+  }
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 });
